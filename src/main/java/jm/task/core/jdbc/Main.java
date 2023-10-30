ce7682a1c5e69cc59a6b3ca71effebceb31d5e98
yastepan <yastepan31@gmail.com>
2023-10-30 15:17:34 +0300

    PP-task1_4 for checking

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,19 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Andrew", "Holywood", (byte)12);
+        userServiceImpl.saveUser("Mikle", "Zotov", (byte)44);
+        userServiceImpl.saveUser("Marina", "Bianka", (byte)33);
+        userServiceImpl.saveUser("Konstantin", "Simonov", (byte)60);
+        userServiceImpl.getAllUsers().stream().forEach(System.out::println);
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    public static final String USR = "rooot";
+    public static final String PSW = "rooot";
+    public static Connection getConnection() {
+        Connection connection = null;
+        try{
+        connection = DriverManager.getConnection(URL,USR,PSW);
+        System.out.println("Connection is opened");
+
+        }
+        catch (SQLException e){
+            System.out.println("Connection is NOT opened");
+        }
+        return connection;
+    }
 }
